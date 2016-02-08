ec029fbfb611c66a0390fb5ff8056318651095ec
Ben W <reccanti@gmail.com>
2016-02-08 10:12:54 -0500

    Initial Commit

---
+++ LightsOut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LightsOut
+//
+//  Created by Benjamin Wilcox on 2/8/16.
+//  Copyright © 2016 Benjamin Wilcox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
